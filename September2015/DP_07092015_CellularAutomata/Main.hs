5bdfc34f475e9e85d5d7f58c66eb165fbc29b19c
Sebastian Luberriaga <sebastian.luberriaga@tiarg.com.ar>
2015-09-08 18:06:44 -0300

    Actualizacion prettyprint

--- September2015/DP_07092015_CellularAutomata/Main.hs
+++ September2015/DP_07092015_CellularAutomata/Main.hs
@@ -1,7 +1,6 @@
 module CA where
 
 import Data.List
-import Data.Bits
 import Data.Char
 
 data Cell = Cell Int deriving (Show)
@@ -9,18 +8,20 @@ data Cell = Cell Int deriving (Show)
 getValue  :: Cell -> Int  
 getValue  (Cell val) = val  
   
-computeNextValue (Cell 1) (Cell a) (Cell 1) = Cell 0
-computeNextValue (Cell 0) (Cell a) (Cell 0) = Cell 0
-computeNextValue (Cell 1) (Cell a) (Cell 0) = Cell 1
-computeNextValue (Cell 0) (Cell a) (Cell 1) = Cell 1
+xor a b = mod ( a + b ) 2
 
-printNGenerationsFromString n s = printNGenerations n ([Cell 0] ++ [ Cell (digitToInt cell) | cell <- s ] ++ [Cell 0]) 
+prettyprint 0 = '_'
+prettyprint 1 = 'x'
+
+computeNextValue (Cell a) (Cell b) = Cell (xor a b)
+
+printNGenerationsFromString n s = printNGenerations n (map Cell ([0] ++ map digitToInt s ++ [0])) 
 
 printNGenerations 0 _ = putStrLn ""
 printNGenerations n ca = do 
-		putStrLn $ show $ currentValues ca
+		putStrLn $ map prettyprint (currentValues ca)
 		printNGenerations (n - 1) (computeNextGeneration ca)
 
 currentValues ca = [ getValue cell | cell <- init(tail(ca)) ]
 
-computeNextGeneration ca = [Cell 0] ++ [ computeNextValue (ca!!(i - 1)) (ca !! i) (ca!!(i + 1))  | i <- [1..(length ca - 2)]] ++ [Cell 0]
+computeNextGeneration ca = [Cell 0] ++ [computeNextValue (ca!!(i - 1)) (ca!!(i + 1))  | i <- [1..(length ca - 2)]] ++ [Cell 0]
