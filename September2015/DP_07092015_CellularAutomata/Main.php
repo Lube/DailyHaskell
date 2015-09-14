9aa33657ff53a9bc53586471b4d8105a7ab4abe7
Sebastian Luberriaga <sebastian.luberriaga@tiarg.com.ar>
2015-09-14 20:03:01 -0300

    Palindromo

--- September2015/DP_07092015_CellularAutomata/Main.php
+++ September2015/DP_07092015_CellularAutomata/Main.php
@@ -1,19 +1,20 @@
 <?php
 function exor($a, $b) {
-	if ($a xor $b)
+	if ($a xor $b){
 		return '1';
+	}
 	return '0';
 }
 
 function getNextGeneration($generation) 
 {	
-	
-	$nextGen[] = exor("0" , $generation[0]); 
-	for ($i=1; $i < (count($generation) - 1) ; $i++) 
+        	
+	$nextGen[] = exor("0", $generation[1]);
+	for ($i=1; $i <= count($generation)-2 ; $i++) 
 	{ 
 	 	$nextGen[] = exor($generation[$i-1], $generation[$i+1]);
 	}
-	$nextGen[] = exor($generation[count($generation) - 1], "0");
+	$nextGen[] = exor($generation[count($generation) - 2], "0");
 
 	return $nextGen; 
 }
@@ -21,22 +22,24 @@ function getNextGeneration($generation)
 function getNGenerations($n, $input) 
 {
 	$input = str_split($input);
-	$finalresult[] = prettyprint($input);
+	$finalRes[] = prettyprint($input);
+	echo $finalRes[0];
+	echo "\n";
 	for ($i=0; $i < $n; $i++) 
 	{ 
 		$input = getNextGeneration($input);
-		$finalresult[] = prettyprint($input);
-	}
-
-	var_dump($finalresult);
+		$finalRes[] = prettyprint($input);
+		echo $finalRes[$i+1];
+		echo "\n";
+	}	
 }
 
 function prettyprint($generation) 
 {
 	$ugly   = array("0", "1");
-	$pretty = array("O", "X");
+	$pretty = array(" ", "X");
 
 	return str_replace($ugly, $pretty, implode("", $generation));
 }
 
-getNGenerations(2, "0101010");
+getNGenerations(25, "0000000000000000000000000000100000000000000000000000000000");
