886927b59878ca120717d686e020414340902ab2
Sebastian Luberriaga <sebastian.luberriaga@tiarg.com.ar>
2015-09-15 18:02:27 -0300

    Lab Diag

--- September2015/1409_LaberintosDiagonales/Main.hs
+++ September2015/1409_LaberintosDiagonales/Main.hs
@@ -1,26 +1,30 @@
 import Data.Char
 
 aMaze =["+-+-+-+-+-+",
-	"  |       |",
-	"+ +-+-+ + +",
-	"| |     | |",
-	"+ + + + + +",
-	"|   | |   |",
-	"+-+-+ +-+-+",
-	"|     |   |",
-	"+ + +-+ + +",
-	"| |     |  ",
-	"+-+-+-+-+-+"]
+		"  |       |",
+		"+ +-+-+ + +",
+		"| |     | |",
+		"+ + + + + +",
+		"|   | |   |",
+		"+-+-+ +-+-+",
+		"|     |   |",
+		"+ + +-+ + +",
+		"| |     |  ",
+		"+-+-+-+-+-+"]
 
-modelMaze = map modelRow 
-
-modelRow (x:xs) | x == '|' = "/" ++ modelRow xs
-	        | x == '-' = '\\'  : modelRow xs
-	        | otherwise = ' ' : modelRow xs
+modelRow (x:xs) | x == '|' = "\\" ++ modelRow xs
+		        | x == '-' = '/'  : modelRow xs
+		        | x == '+' =  modelRow xs
+		        | otherwise = ' ' : modelRow xs
 
 modelRow _ = []
 
-shiftMaze maze = zipWith (++) (modelMaze maze) [concat(replicate n " ") | let l = (length maze) `div` 2, n <- ([l, l-1..0] ++ [1,2..l])]
+buildRow matrix b = [ if (hasCoord off a b ancho alto) then ' ' else  (matrix!!x)!!y | let ancho = length matrix, let alto = (length . head $ matrix), let off = ((ancho+alto-1) `div` 2), a <- [1,3..ancho+alto-2], let x = (a + b  - off)`div` 2, let y = b - (a + b  - off)`div` 2]
 
+hasCoord  off a b ancho alto | ((a + b  - off) `mod` 2 /= 0)  = True
+							 | ((a + b  - off) `div` 2 >= 0) && ((a + b  - off) `div` 2 < ancho) && (b - (a + b  - off)`div` 2 >= 0) && (b - (a + b  - off)`div` 2) < alto  = False
+							 | otherwise           = True
 
-transpose maze = [ (-a+b+((xlen+ylen-1)`div`2), a+b `div` 2, ((maze!!b)!!a)) | let ylen = (length maze), let xlen = (length . head $ maze), a <- [0..ylen-1], b <- [0..xlen-1]]
+buildMaze = map (buildRow aMaze) [1,3..(length aMaze + (length . head) aMaze - 1)]
+
+showMaze = mapM putStrLn (map modelRow buildMaze)
